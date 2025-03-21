443da81615fe2347ac16eeecb6bc14f649d07100
lMatteo0311 <levaimate03@gmail.com>
2025-03-21 13:35:11 +0100

    Add project files.

---
+++ 1_feladat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> szamok = new List<int>();
+            int[] p = { 12, 10, 38, 1, -6, 23, 9, 15};
+            Console.WriteLine("A lista elemei: ");
+            Console.WriteLine(p);
+            
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                
+            }
+            
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 3_feladat/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> szamok = new List<int>();
+            Random rnd = new Random();
+            listaFeltolt(10);
+
+            listaAtlag(szamok);
+            Console.ReadKey();
+        }
+
+            static List<int> listaFeltolt(int v)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int vszam = rnd.Next(10, 101);
+                szamok.Add(vszam);
+                Console.Write(vszam + " ");
+            }
+            return v;
+        }
+
+        static void listaAtlag(List<double> szamok)
+        {
+            double szamAtlag = szamok.Average();
+            Console.Write($"Átlag: {szamAtlag}"); ;
+
+
+        }
+    }
+}
